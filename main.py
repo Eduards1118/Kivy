ab4340b882a835ea36ced26cc146d9465df77050
Eduards Paradniks <eduards.paradniks@dpolvsk.lv>
2026-05-08 10:22:02 +0300

    izmainas + pievienoju failu parole.py

--- main.py
+++ main.py
@@ -8,38 +8,39 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-
+        # Galvenais logs
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
-
+        # Teksta ievade
         self.input_text = TextInput(
             hint_text='Ievadiet tekstu',
             multiline=False,
             size_hint=(1, 0.2)
         )
 
-
+        # Teksta lauks
         self.label = Label(
             text='Šeit parādīsies teksts',
             size_hint=(1, 0.4)
         )
 
-
+        # Poga
         send_button = Button(
             text='Sūtīt',
             size_hint=(1, 0.2)
         )
 
+        # Pogas darbība
         send_button.bind(on_press=self.send_text)
 
-
+        # Elementu pievienošana
         layout.add_widget(self.input_text)
         layout.add_widget(send_button)
         layout.add_widget(self.label)
 
         return layout
 
-
+    # Teksta nosūtīšana
     def send_text(self, instance):
         self.label.text = self.input_text.text
 
---
+++ parole.py
@@ -0,0 +1,66 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+import random
+import string
+
+
+class MyApp(App):
+
+    def build(self):
+        layout = BoxLayout(
+            orientation='vertical',
+            padding=10,
+            spacing=10
+        )
+
+        self.input_text = TextInput(
+            hint_text='Ģenerētā parole',
+            multiline=False,
+            size_hint=(1, 0.2)
+        )
+
+        self.label = Label(
+            text='Nospied pogu paroles ģenerēšanai',
+            size_hint=(1, 0.3)
+        )
+
+        generate_button = Button(
+            text='Ģenerēt paroli',
+            size_hint=(1, 0.2)
+        )
+
+        parbaudit_button = Button(
+            text='Parbaudīt paroli',
+            size_hint=(1, 0.2)
+        )
+
+        generate_button.bind(on_press=self.generate_password)
+
+        layout.add_widget(self.input_text)
+        layout.add_widget(generate_button)
+        layout.add_widget(parbaudit_button)
+        layout.add_widget(self.label)
+
+        return layout
+
+    def generate_password(self, instance):
+        simboli = (
+                string.ascii_letters +
+                string.digits +
+                string.punctuation
+        )
+        parole = ""
+
+        for i in range(8):
+            parole += random.choice(simboli)
+        self.input_text.text = parole
+        self.label.text = "Parole izveidota"
+
+    def check_password(self):
+        parbaude = self.input_text.text()
+
+
+MyApp().run()
